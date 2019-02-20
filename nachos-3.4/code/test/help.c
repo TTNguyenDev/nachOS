b6c34afa14731f9a06e4c7266b5738f9cde73f03
ttnguyenIOS <sv@localhost.localdomain>
2019-02-20 13:24:07 +0700

    help program

--- nachos-3.4/code/test/help.c
+++ nachos-3.4/code/test/help.c
@@ -4,8 +4,8 @@
 
 int main() {
 	PrintString("\t####################### HELP ########################\n");
-	PrintString("\t# Group: DAT VU                                     #\n");
-	PrintString("\t# Member:                                           #\n");
+	PrintString("\t#  Group: DAT VU                                    #\n");
+	PrintString("\t#  Member:                                          #\n");
 	PrintString("\t# 	Nguyen Trong Triet   - 1753113              #\n");
 	PrintString("\t# 	Nguyen Vuong Dat Vu  - 1753131              #\n");
 	PrintString("\t# 	Truong Tran Hai Yen  - 1753139              #\n");
