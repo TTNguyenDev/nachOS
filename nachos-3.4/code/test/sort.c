a29e61324743c6baf7d9bc9f2a7cc4f270e142be
ttnguyenIOS <sv@MiWiFi-R3-srv.(none)>
2019-03-15 17:17:13 +0700

    fix exception error

--- nachos-3.4/code/test/sort.c
+++ nachos-3.4/code/test/sort.c
@@ -1,5 +1,3 @@
-
-
 #include "syscall.h"
 
 int main() {
--- nachos-3.4/code/test/text.c
+++ nachos-3.4/code/test/text.c
@@ -2,11 +2,14 @@
 #define MAX_LENGTH 255
 
 int main() {
-	char* result;
+
+	char* result[MAX_LENGTH];
 	PrintString("--------------------TEXT--------------------\n");
 	PrintString("Enter what you want to print to console: ");
-	ReadString(result, 255);
+	ReadString(result, MAX_LENGTH);
+	PrintString("Output: ");
 	PrintString(result);
 	PrintString("\n");
 	return 0;
+	
 }
