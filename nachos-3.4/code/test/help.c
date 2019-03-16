d2ab6dbcfddaa4fe19bb0b551c3e26f103611635
ttnguyenIOS <sv@localhost.localdomain>
2019-03-16 13:40:26 +0700

    Update Help Program

--- nachos-3.4/code/test/help.c
+++ nachos-3.4/code/test/help.c
@@ -10,6 +10,9 @@ int main() {
 	PrintString("\t# 	Nguyen Vuong Dat Vu  - 1753131              #\n");
 	PrintString("\t# 	Truong Tran Hai Yen  - 1753139              #\n");
 	PrintString("\t####################### HCMUS #######################\n");
-
+	
+	PrintString("\t * Text: Progam allow the input which has maximum length is 255. \n \t The input will be change into string so that the program will print out that string to the console.\n");
+	PrintString("\t * Sort: Program let the user input an array of string. \n \t Using Bubble sort algorithm to sort that array. Finally the program will print out the result to console.\n");
+	
 	return 0;
 }
