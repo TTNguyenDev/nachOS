c93b22d1a66b3f59ed9814011a0568eaf27e739e
Nguyễn Trọng Triết <42999269+TTNguyenDev@users.noreply.github.com>
2019-03-17 10:56:42 +0700

    update

--- nachos-3.4/code/test/help.c
+++ nachos-3.4/code/test/help.c
@@ -9,8 +9,8 @@ int main() {
 	PrintString("\t# 	Truong Tran Hai Yen  - 1753139              #\n");
 	PrintString("\t####################### HCMUS #######################\n");
 	
-	PrintString("\t * Text: Program allow user enter a string which has maximum length is 255. \n \t The input will be loaded by ReadString syscall. \n \t After that the program will print out that string to the console with PrintString syscall.\n");
-	PrintString("\t * Sort: Program let user input an array of string. Using ReadInt syscall to read the input \n \t and use Bubble sort algorithm to sort that array. \n \t Finally the program will print out the sorted array to console with PrintInt syscall.\n");
+	PrintString("\t * Text: Program allow user enter a string \n \t which has maximum length is 255. \n \t The input will be loaded by ReadString syscall. \n \t After that the program will print out that string \n \t to the console with PrintString syscall.\n");
+	PrintString("\t * Sort: Program let user input an array of string. \n \t Using ReadInt syscall to read the input \n \t and use Bubble sort algorithm to sort that array. \n \t Finally the program will print out \n \t the sorted array to console with PrintInt syscall.\n");
 	
 	return 0;
 }
